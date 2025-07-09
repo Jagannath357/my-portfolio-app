f77043a1dda588d04e20bb193af165967a2e5519
Jagannath357 <padhijaga200@gmail.com>
2025-07-09 14:59:59 +0530

    file added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,13 @@
 function About() {
     return ( 
-        <div className="container-fluid d-flex justify-content-center mt-5 mb-5" id="aboutContainer">
+        <div className="container d-flex justify-content-center mt-5 mb-5" id="aboutContainer">
             <div className="col-12 d-flex justify-content-center align-items-center">
                 <div className="row gap-5">
                     <div className="col-12">
                         <h1 className="text-center">About Me</h1>
                         <p className="ps-2 pe-2 text-center fs-5">
-                            {/* I'm a passionate full-stack developer with 3+ years of experience creating web applications that solve real-world problems. I love working with modern technologies and am always eager to learn new things. */}
-                            I'm currently pursuing a degree in Computer Science, and over the past year, I've developed a strong passion for front-end development. I enjoy transforming design concepts into clean, responsive, and user-friendly websites using HTML, CSS, JavaScript, and React.
+                            
+                            I'm currently pursuing a degree in Computer Science, and over the past year, I've developed a strong passion for front-end development. I enjoy transforming design concepts into clean, responsive, and user-friendly websites using HTML, CSS, JavaScript, and React. <br /> <br />
                             I've built several personal projects, including my own portfolio website, a to-do list app, and a personal expense tracker. I also collaborated with a team to redesign a university club’s website, which helped me strengthen my teamwork and communication skills.
 
                         </p>
